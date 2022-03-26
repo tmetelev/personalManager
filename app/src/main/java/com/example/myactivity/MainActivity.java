b1c8a7cf267b2691d16d9f2f115cd60ff65c5021
TMetelev <t.metelew@gmail.com>
2022-03-27 00:05:24 +0300

    Undercut

--- app/src/main/java/com/example/myactivity/MainActivity.java
+++ app/src/main/java/com/example/myactivity/MainActivity.java
@@ -9,60 +9,15 @@ import android.widget.ListView;
 import android.widget.Toast;
 
 import com.example.myactivity.misc.JSONHelper;
-import com.example.myactivity.structures.User;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-
-    private ArrayAdapter<User> adapter;
-    private EditText nameText, ageText;
-    private List<User> users;
-    ListView listView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        nameText = findViewById(R.id.nameText);
-        ageText = findViewById(R.id.ageText);
-        listView = findViewById(R.id.list);
-        users = new ArrayList<>();
-
-        adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, users);
-        listView.setAdapter(adapter);
-
-        open(listView);
-    }
-
-    public void addUser(View view){
-        String name = nameText.getText().toString();
-        int age = Integer.parseInt(ageText.getText().toString());
-        User user = new User(name, age);
-        users.add(user);
-        adapter.notifyDataSetChanged();
-    }
-
-    public void save(View view){
-
-        boolean result = JSONHelper.exportToJSON(this, users);
-        if(result){
-            Toast.makeText(this, "Данные сохранены", Toast.LENGTH_LONG).show();
-        }
-        else{
-            Toast.makeText(this, "Не удалось сохранить данные", Toast.LENGTH_LONG).show();
-        }
-    }
-    public void open(View view){
-        users = JSONHelper.importFromJSON(this);
-        if(users!=null){
-            adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, users);
-            listView.setAdapter(adapter);
-            Toast.makeText(this, "Данные восстановлены", Toast.LENGTH_LONG).show();
-        }
-        else{
-            Toast.makeText(this, "Не удалось открыть данные", Toast.LENGTH_LONG).show();
-        }
     }
 }
--- app/src/main/java/com/example/myactivity/misc/JSONHelper.java
+++ app/src/main/java/com/example/myactivity/misc/JSONHelper.java
@@ -2,7 +2,6 @@ package com.example.myactivity.misc;
 
 import android.content.Context;
 
-import com.example.myactivity.structures.User;
 import com.google.gson.Gson;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
@@ -12,50 +11,50 @@ import java.util.List;
 
 public class JSONHelper {
 
-    private static final String FILE_NAME = "data.json";
-
-    public static boolean exportToJSON(Context context, List<User> dataList) {
-
-        Gson gson = new Gson();
-        DataItems dataItems = new DataItems();
-        dataItems.setUsers(dataList);
-        String jsonString = gson.toJson(dataItems);
-
-        try(FileOutputStream fileOutputStream =
-                    context.openFileOutput(FILE_NAME, Context.MODE_PRIVATE)) {
-            fileOutputStream.write(jsonString.getBytes());
-            return true;
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-        return false;
-    }
-
-    public static List<User> importFromJSON(Context context) {
-
-        try(FileInputStream fileInputStream = context.openFileInput(FILE_NAME);
-            InputStreamReader streamReader = new InputStreamReader(fileInputStream)){
-
-            Gson gson = new Gson();
-            DataItems dataItems = gson.fromJson(streamReader, DataItems.class);
-            return  dataItems.getUsers();
-        }
-        catch (IOException ex){
-            ex.printStackTrace();
-        }
-
-        return null;
-    }
-
-    private static class DataItems {
-        private List<User> users;
-
-        List<User> getUsers() {
-            return users;
-        }
-        void setUsers(List<User> users) {
-            this.users = users;
-        }
-    }
+//    private static final String FILE_NAME = "data.json";
+//
+//    public static boolean exportToJSON(Context context, List<User> dataList) {
+//
+//        Gson gson = new Gson();
+//        DataItems dataItems = new DataItems();
+//        dataItems.setUsers(dataList);
+//        String jsonString = gson.toJson(dataItems);
+//
+//        try(FileOutputStream fileOutputStream =
+//                    context.openFileOutput(FILE_NAME, Context.MODE_PRIVATE)) {
+//            fileOutputStream.write(jsonString.getBytes());
+//            return true;
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//
+//        return false;
+//    }
+//
+//    public static List<User> importFromJSON(Context context) {
+//
+//        try(FileInputStream fileInputStream = context.openFileInput(FILE_NAME);
+//            InputStreamReader streamReader = new InputStreamReader(fileInputStream)){
+//
+//            Gson gson = new Gson();
+//            DataItems dataItems = gson.fromJson(streamReader, DataItems.class);
+//            return  dataItems.getUsers();
+//        }
+//        catch (IOException ex){
+//            ex.printStackTrace();
+//        }
+//
+//        return null;
+//    }
+//
+//    private static class DataItems {
+//        private List<User> users;
+//
+//        List<User> getUsers() {
+//            return users;
+//        }
+//        void setUsers(List<User> users) {
+//            this.users = users;
+//        }
+//    }
 }
