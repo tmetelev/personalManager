1a6c4442d57549403cb009a160a7ede051fc0180
TMetelev <t.metelew@gmail.com>
2022-03-27 16:10:10 +0300

    workable task adding

--- app/src/main/java/com/example/myactivity/activities/oneproject/OneprojectActivity.java
+++ app/src/main/java/com/example/myactivity/activities/oneproject/OneprojectActivity.java
@@ -4,10 +4,16 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CalendarView;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.myactivity.R;
 import com.example.myactivity.activities.projects.ProjectsRecyclerAdapter;
@@ -21,10 +27,14 @@ import java.util.List;
 public class OneprojectActivity extends AppCompatActivity {
     private Button addButton;
     private EditText newNameText;
+    private CalendarView calendarView;
     private RecyclerView recyclerView;
     private OneprojectRecyclerAdapter adapter;
     private List<Task> tasks = new ArrayList<>();
     private String dataFileName = "";
+    private String selectedDate = "010122";
+    private Task addableTask;
+    private Context context = this;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,7 +44,6 @@ public class OneprojectActivity extends AppCompatActivity {
         dataFileName = getIntent().getExtras().getString("file");
 
         addButton = findViewById(R.id.op_add_button);
-        newNameText = findViewById(R.id.op_new_t_name);
 
         open();
         recyclerView = findViewById(R.id.op_recyclerView);
@@ -44,18 +53,53 @@ public class OneprojectActivity extends AppCompatActivity {
     }
 
     public void add(View view) {
-        tasks = adapter.getTasks();
-        Task project = new Task(newNameText.getText().toString(), "121122", "1200");
-        tasks.add(project);
-        boolean result = JSONHelper.exportToJSON(this, tasks, dataFileName);
-        if(result){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        LayoutInflater li = LayoutInflater.from(this);
+        View windowView = li.inflate(R.layout.oneproject_add_dialog, null);
+        builder.setView(windowView);
+        newNameText = windowView.findViewById(R.id.op_add_name);
+        calendarView = windowView.findViewById(R.id.op_add_calendar);
+        calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(CalendarView view, int year, int month, int dayOfMonth) {
+                String mYear = Integer.toString(year).substring(2, 4);;
+                String mMonth = Integer.toString(month + 1);
+                if (mMonth.length() == 1)
+                    mMonth = "0" + mMonth;
+                String mDay = Integer.toString(dayOfMonth);
+                if (mDay.length() == 1)
+                    mDay = "0" + mDay;
+                selectedDate = mDay + mMonth + mYear;
+                Toast.makeText(getApplicationContext(), selectedDate, Toast.LENGTH_LONG).show();
+            }
+        });
+        builder
+                .setTitle("Add new task")
+                .setPositiveButton("Add", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        addableTask = new Task(newNameText.getText().toString(), selectedDate, "0000");
+                        tasks = adapter.getTasks();
+                        tasks.add(addableTask);
+                        boolean result = JSONHelper.exportToJSON(context, tasks, dataFileName);
+                        if(result){
 //            Toast.makeText(this, "Данные сохранены", Toast.LENGTH_LONG).show();
-        }
-        else{
+                        }
+                        else{
 //            Toast.makeText(this, "Не удалось сохранить данные", Toast.LENGTH_LONG).show();
-        }
-        recyclerUpdate();
-        newNameText.setText("");
+                        }
+                        recyclerUpdate();
+                        dialogInterface.cancel();
+                    }
+                })
+                .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        dialogInterface.cancel();
+                    }
+                });
+        AlertDialog alertDialog = builder.create();
+        alertDialog.show();
     }
 
     public void open() {
