f2535fb647f6958499654ffa52f091063e06915d
TMetelev <t.metelew@gmail.com>
2022-07-14 21:10:27 +0300

    list of day is showing

--- app/src/main/java/com/example/myactivity/activities/day/DayActivity.java
+++ app/src/main/java/com/example/myactivity/activities/day/DayActivity.java
@@ -1,10 +1,23 @@
 package com.example.myactivity.activities.day;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.example.myactivity.R;
+import com.example.myactivity.activities.calendar.CalendarRecyclerAdapter;
+import com.example.myactivity.misc.Utilities;
+import com.example.myactivity.structures.Project;
+import com.example.myactivity.structures.Task;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
 
 public class DayActivity extends AppCompatActivity {
 
@@ -14,6 +27,22 @@ public class DayActivity extends AppCompatActivity {
         setContentView(R.layout.activity_day);
         setTitle("Timetable");
 
+        List<Project> projects = Utilities.getProjects(this);
 
+        DateFormat df = new SimpleDateFormat("yyMMdd");
+        String date = df.format(Calendar.getInstance().getTime());
+
+        List<Task> tasks = new ArrayList<>();
+        for (Project project : projects) {
+            List<Task> t = Utilities.getTasksOfThisDay(this, project, date);
+            if (t.size() != 0)
+                tasks.addAll(t);
+        }
+
+//        Toast.makeText(this, Integer.toString(tasks.size()), Toast.LENGTH_SHORT).show();
+        RecyclerView recyclerView = findViewById(R.id.day_recyclerView);
+        DayRecyclerAdapter adapter = new DayRecyclerAdapter( this, tasks);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(adapter);
     }
 }
---
+++ app/src/main/java/com/example/myactivity/activities/day/DayRecyclerAdapter.java
@@ -0,0 +1,75 @@
+package com.example.myactivity.activities.day;
+
+import android.annotation.SuppressLint;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.Color;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.myactivity.R;
+import com.example.myactivity.activities.taskinfo.TaskInfoActivity;
+import com.example.myactivity.misc.JSONHelper;
+import com.example.myactivity.structures.Project;
+import com.example.myactivity.structures.Task;
+
+import java.util.List;
+
+public class DayRecyclerAdapter extends RecyclerView.Adapter<DayRecyclerAdapter.MyViewHolder> {
+    private List<Task> tasks;
+    private Context context;
+
+    public DayRecyclerAdapter(Context context, List<Task> tasks) {
+        this.context = context;
+        this.tasks = tasks;
+    }
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.day_item, parent, false);
+        return new MyViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder holder, @SuppressLint("RecyclerView") int position) {
+        holder.nameView.setText(tasks.get(position).getName());
+        holder.timeView.setText(tasks.get(position).getTime());
+        holder.cardView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(context, TaskInfoActivity.class);
+                intent.putExtra("name", tasks.get(position).getTag() + ".json");
+                intent.putExtra("pos", position);
+                context.startActivity(intent);
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return tasks.size();
+    }
+
+    class MyViewHolder extends RecyclerView.ViewHolder {
+        private TextView nameView;
+        private CardView cardView;
+        private TextView timeView;
+
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+            nameView = (TextView) itemView.findViewById(R.id.day_item_name);
+            cardView = (CardView) itemView.findViewById(R.id.day_item_card);
+            timeView = (TextView) itemView.findViewById(R.id.day_item_time);
+        }
+    }
+}
